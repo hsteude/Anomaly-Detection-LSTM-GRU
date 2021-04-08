dd8f43505fd82fc667f097acb873247540969c68
Henrik <henrik.steude@ginkgo-analytics.com>
2021-04-08 17:35:59 +0200

    set up repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Some experiments with LSTMs and GRUs for anomaly detection use cases on publicly available dataset(s).',
+    author='Henrik Steude',
+    license='',
+)
