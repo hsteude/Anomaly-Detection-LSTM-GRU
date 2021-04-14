77ee5755dc4c41c3f158fe07d72fbcfa58256c2a
henrik <henrik.steude@gmail.com>
2021-04-14 19:27:01 +0200

    time series exploration, and other smaller stuff

--- anodeclstmgru/constants.py
+++ anodeclstmgru/constants.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 # PATHS
 DATA_FOLDER_PATH = os.path.join('.', 'data', 'interim',
@@ -9,5 +10,9 @@ LABELS_FILE_PATH = os.path.join(DATA_FOLDER_PATH, 'List_of_attacks_Final.xlsx')
 PHYS_NORMAL_PATHS = [
     os.path.join(PHYSICAL_FOLDER_PATH, f)
     for f in ['SWaT_Dataset_Normal_v0.xlsx', 'SWaT_Dataset_Normal_v1.xlsx']]
-PHYS_ATTACK_PATH = os.path.join(PHYSICAL_FOLDER_PATH, 'SWaT_Dataset_Attack_v0.xlsx')
+PHYS_ATTACK_PATH = os.path.join(
+    PHYSICAL_FOLDER_PATH, 'SWaT_Dataset_Attack_v0.xlsx')
 HDF_STORE_PATH = os.path.join(DATA_FOLDER_PATH, 'data_store.h5')
+
+MIN_DATE = datetime(2015, 12, 22)
+MAX_DATE = datetime(2016, 1, 2)
